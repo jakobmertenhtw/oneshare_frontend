6fd83fb163be53a9a084269447855bb5e1c95dc3
= <147500519+jakobmertenhtw@users.noreply.github.com>
2023-12-24 13:30:12 +0100

    adds profile information view

--- src/components/LoadingComponent.vue
+++ src/components/LoadingComponent.vue
@@ -11,12 +11,26 @@ export default {
       default: "#fff",
     },
   },
+  computed: {
+    spinnerSize() {
+      switch (this.size) {
+        case "small":
+          return "20px";
+        case "medium":
+          return "30px";
+        case "large":
+          return "40px";
+        default:
+          return "30px";
+      }
+    }
+  }
 };
 </script>
 
 <template>
 
-  <div class="loading-container" :style="`--color: ${color}`">
+  <div class="loading-container" :style="`--color: ${color}; --size: ${spinnerSize}`">
     <span></span>
   </div>
 </template>
@@ -24,8 +38,8 @@ export default {
 <style scoped>
 .loading-container span {
     display: block;
-  width: 30px;
-  height: 30px;
+    width: var(--size, 30px);
+    height: var(--size, 30px);
   border: 3px solid transparent;
   border-radius: 50%;
   border-right-color: var(--color, #fff);
--- src/views/UserInformationView.vue
+++ src/views/UserInformationView.vue
@@ -1,19 +1,103 @@
-<template>
-    <h1>Profile Information</h1>
-</template>
-
 <script>
+import LoadingComponent from "@/components/LoadingComponent.vue";
 export default {
   data() {
     return {
-      title: 'Hello Vue!',
-      message: 'Welcome to my Vue app!',
-      counter: 0
+      user: {
+        id: 1,
+        firstname: "Test",
+        lastname: "User",
+        email: "test@mail.com",
+        phoneNumber: "1234567890",
+        password: "password",
+      },
+      isLoading: false,
     };
   },
+  methods: {
+    save() {
+      this.isLoading = true;
+    }
+  }, 
+  components: {
+    LoadingComponent
+  }
 };
 </script>
-
+<template>
+  <div class="main-container-user-info">
+    <h1>Profile Information</h1>
+    <form action="">
+      <div class="name-info">
+        <input type="text" placeholder="Firstname" />
+        <input type="text" placeholder="Lastname" />
+      </div>
+      <input type="email" placeholder="Email" />
+      <input type="text" placeholder="Telephone Number" />
+      <input type="password" placeholder="Passwort" />
+    </form>
+    <div class="buttons-container">
+      <button class="save-button" @click="save">
+        <p v-if="!isLoading">SAVE</p>
+        <LoadingComponent size="small" v-if="isLoading" />
+      </button>
+      <button class="dismiss-button">DISMISS</button>
+    </div>
+  </div>
+</template>
 <style scoped>
+.main-container-user-info {
+  padding: 7rem 4rem;
+}
+h1 {
+  font-size: 2.1rem;
+}
+form {
+  margin-top: 2rem;
+  margin-bottom: 1rem ;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+form input {
+  padding: 1.2rem 2rem;
+  border: none;
+  outline: none;
+  background-color: #fff;
+  color: #222;
+  font-family: 'Montserrat', sans-serif;
+  border-radius: 15px;
+}
+.name-info {
+  display: flex;
+  gap: 1rem;
+}
+.name-info input {
+  flex: 1;
+}
+input:focus {
+  background-color: rgba(0, 0, 0, .025);
+}
+.buttons-container {
+  margin-top: 2rem;
+  display: flex;
+  gap: .75rem;
+}
+.save-button {
+  background-color: #222;
+  color: white;
+  border: none;
+  outline: none;
+  min-width: 6rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.dismiss-button {
+  border: 2px solid #222;
+  color: #222;
+}
+
+
 
 </style>
